3dea868d6ef9296be5913dc06657a5458e09e790
Jonas Henriksson <jonas.henriksson@hiq.se>
2019-01-01 18:37:12 +0100

    commit from command line

---
+++ src/main/java/getCard/GetCardController.java
@@ -0,0 +1,19 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GetCardController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/getCard")
+    public GetCard getCard(@RequestParam(value="name", defaultValue="World") String name) {
+        return new GetCard(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
